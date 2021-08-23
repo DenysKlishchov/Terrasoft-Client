69d338a7a9be1b52b9ab8937396d9907eefaa419
Denys Klischov <dklischovfg@gmail.com>
2021-08-23 16:57:43 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ASPControlPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.AnalysisBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(30, 26);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.openFileBtn.TabIndex = 0;
+            this.openFileBtn.Text = "Открыть";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(433, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // AnalysisBtn
+            // 
+            this.AnalysisBtn.Location = new System.Drawing.Point(199, 68);
+            this.AnalysisBtn.Name = "AnalysisBtn";
+            this.AnalysisBtn.Size = new System.Drawing.Size(174, 23);
+            this.AnalysisBtn.TabIndex = 2;
+            this.AnalysisBtn.Text = "Анализ";
+            this.AnalysisBtn.UseVisualStyleBackColor = true;
+            this.AnalysisBtn.Click += new System.EventHandler(this.analysisBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 114);
+            this.Controls.Add(this.AnalysisBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openFileBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button AnalysisBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ASPControlPC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (openFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            
+            textBox1.Text = openFile.FileName;
+        }
+
+        private async void analysisBtn_Click(object sender, EventArgs e)
+        {
+            string JSONData = await Task.Factory.StartNew(() => JsonConvert.SerializeObject(textBox1.Text));
+            WebRequest request = WebRequest.Create("https://localhost:44317/Home/GetMetricsAsync");
+            request.Method = "POST";
+
+            string query = $"path={JSONData}";
+
+            byte[] byteMass = Encoding.UTF8.GetBytes(query);
+
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteMass.Length;
+
+            using (Stream stream = await request.GetRequestStreamAsync())
+            {
+                await stream.WriteAsync(byteMass, 0, byteMass.Length);
+            }
+
+            WebResponse response = await request.GetResponseAsync();
+
+            string answer = null;
+
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader sR = new StreamReader(s)) 
+                {
+                    answer = await sR.ReadToEndAsync();
+                }
+            }
+            response.Close();
+
+            MessageBox.Show(answer);
+
+        }
+    }
+}
